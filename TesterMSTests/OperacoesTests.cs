87dec633c827d90aa371474dafff6d72fa3f7a74
Leory2 <>
2024-11-12 11:17:29 -0300

    update

---
+++ TesterMSTests/OperacoesTests.cs
@@ -0,0 +1,24 @@
+using TesterCore;
+
+namespace TesterMSTests
+{   
+        [TestClass]
+        public class OperacoesTests
+        {
+            [TestMethod]
+            public void SomarDoisNumeroRetornarResultado()
+            {
+                //Arrange
+                double primeiroNumero = 10;
+                double segundoNumero = 20;
+
+                //act
+                var resultado = Operacoes.Somar(primeiroNumero, segundoNumero);
+
+                //Assert
+                Assert.AreEqual(30, resultado);
+
+
+            }
+        }
+    }
